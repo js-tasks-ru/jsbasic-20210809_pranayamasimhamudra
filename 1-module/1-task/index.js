9a64fd78bca89be7391e1bb5eed14537b5ba9b97
Ole <pranayamasimhamudra@gmail.com>
2021-08-15 01:08:26 +0500

    solution 1st task of 1st module - wrote factorial function

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
   // ваш код...
-}
+  let result=1;
+  if (n==0){
+    return result;
+  } else if (n<0) {
+    return result = console.log('Факториал - функция, определенная на множестве неотрицательных чисел');
+  }
+  else {
+    for(n;n>0;n--){
+      result *= n;
+    }
+    return result;
+  }
+}     
