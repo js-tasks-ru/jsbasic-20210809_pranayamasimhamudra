122c23bd8409cee8ccccb9905241077a59e1640b
Ole <pranayamasimhamudra@gmail.com>
2021-08-25 14:09:25 +0500

    solution for 4th task of module-3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
   // ваш код...
+  let result = '';
+  for (let i=0; i < users.length; i++) {
+    const user = users[i];
+    if (user.age <= age) {
+      result += user.name + ', ' + user.balance + '\n';}} 
+  return result.slice(0,-1);
 }
