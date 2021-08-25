6538385abd14c2ba769835df39eb6bdde74d1225
Ole <pranayamasimhamudra@gmail.com>
2021-08-25 21:15:16 +0500

    solution for 5th task of module-3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
   // ваш код...
+  let arr = str.split(' ');
+  let array=[];
+  for (let i=0; i < arr.length; i++){
+    if ( !isNaN(arr[i])) {
+      array.splice(0,0, arr[i]);
+    }
+  }
+  let min = Math.min(...array);
+  let max = Math.max(...array);
+  let result = {
+    min: min,
+    max: max,
+  }
+  return result;
 }
