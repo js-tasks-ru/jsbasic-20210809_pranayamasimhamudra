3f0ab4f679c3967994dda180c65ba899a59eaef2
Ole <pranayamasimhamudra@gmail.com>
2021-08-31 22:39:35 +0500

    solution for 2nd task of module-4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
   // ваш код...
+  for (let i=0; i < table.rows.length; i++) {
+    const td = table.rows[i].cells[i];
+    td.style.backgroundColor = 'red';
+  }
 }
