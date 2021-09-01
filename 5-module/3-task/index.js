e3cab51d5753434f90404e42746b2502aaaf35ca
Ole <pranayamasimhamudra@gmail.com>
2021-09-02 03:41:54 +0500

    solution for 3rd task of module-5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
   // ваш код...
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const slides = document.querySelector('.carousel__inner');
+  const slide = document.querySelector('.carousel__slide');
+  //const slidesCount = document.querySelectorAll('.carousel__inner').length;
+  let offset = slides.offsetWidth;
+  let activeSlide = 1;
+  arrowLeft.style.display = 'none';
+  let position = 0;
+
+  function changeSlideRight() {
+    position = position - offset;
+    slides.style.transform = `translateX(${position}px)`;
+    arrowLeft.style.display = '';
+    activeSlide++;
+    if (activeSlide >= 4) {
+      activeSlide = 4;
+      arrowRight.style.display = 'none';
+    }
+  }
+
+  function changeSlideLeft() {
+    position = position + offset;
+    slides.style.transform = `translateX(${position}px)`;
+    arrowRight.style.display = '';
+    activeSlide--;
+    if (activeSlide <= 0) {
+      activeSlide = 0;
+      arrowLeft.style.display = 'none';
+    }
+  }
+
+  arrowRight.addEventListener('click', () => {
+    changeSlideRight();
+  });
+
+  arrowLeft.addEventListener('click', () => {
+    changeSlideLeft();
+  } )
 }
