25bf3d2adad0e74c5133b8fc367fe57c0b265fb8
Ole <pranayamasimhamudra@gmail.com>
2021-09-02 00:09:16 +0500

    solution for 3rd task of module-4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
   // ваш код...
+  for (let i=0; i < table.rows.length; i++) {
+    const status = table.rows[i].cells[3];
+    const gender = table.rows[i].cells[2].innerHTML;
+    const age = table.rows[i].cells[1].innerHTML;
+    if (status.getAttribute('data-available') === "true") {
+      table.rows[i].classList.add('available');
+  } else if (status.getAttribute('data-available') === "false") {
+    table.rows[i].classList.add('unavailable');
+  } else {
+    table.rows[i].setAttribute('hidden', '');}
+    if (gender === 'm') {
+      table.rows[i].classList.add('male');
+    } else if (gender === 'f') {
+      table.rows[i].classList.add('female');
+    }
+    if (age < '18') {
+      table.rows[i].style.textDecoration = 'line-through';
+    }
+  } 
 }
