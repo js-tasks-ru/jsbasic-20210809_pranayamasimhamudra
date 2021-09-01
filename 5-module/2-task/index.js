b6556f9ebab9bbc2e10131a6bd4f6d270c8b93ab
Ole <pranayamasimhamudra@gmail.com>
2021-09-02 01:22:45 +0500

    solution for 2nd task of module-5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
   // ваш код...
+  const btnHide = document.querySelector('.toggle-text-button');
+  const text = document.getElementById('text');
+  btnHide.addEventListener('click', () => { 
+    text.toggleAttribute('hidden')});
 }
